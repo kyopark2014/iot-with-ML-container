1ecc8772d7e73a44b544355c748b337637a040e3
ksdyb.park <ksdyb@amazon.com>
2022-11-10 23:29:38 +0900

    update

--- cdk-ml-iot/lib/cdk-ml-iot-stack.ts
+++ cdk-ml-iot/lib/cdk-ml-iot-stack.ts
@@ -2,34 +2,97 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as path from "path";
 import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
-import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as ecrDeploy from 'cdk-ecr-deployment';
+import * as greengrassv2 from 'aws-cdk-lib/aws-greengrassv2';
 
 export class CdkMlIotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
-    const repo = new ecr.Repository(this, 'IoTRepository', {
-      repositoryName: 'iot_repository',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      imageTagMutability: ecr.TagMutability.IMMUTABLE
-    });
+    const deviceName = 'GreengrassCore-18163f7ac3e'
+    const accountId = cdk.Stack.of(this).account
 
     const asset = new DockerImageAsset(this, 'BuildImage', {
       directory: path.join(__dirname, '../../src/ml-container'),
     })
-    
-    new ecrDeploy.ECRDeployment(this, 'DeployDockerImage', {
-      src: new ecrDeploy.DockerImageName(asset.imageUri),
-      dest: new ecrDeploy.DockerImageName(`${repo.repositoryUri}:latest`),
+
+    const imageUri = asset.imageUri
+    new cdk.CfnOutput(this, 'ImageUri', {
+      value: imageUri,
+      description: 'Image Uri',
     }); 
 
-    repo.addLifecycleRule({ tagPrefixList: ['dev'], maxImageCount: 9999 });
-    repo.addLifecycleRule({ maxImageAge: cdk.Duration.days(30) });
+    // recipe of component - com.ml.xgboost
+    const version = "1.0.0"
+    const recipe = `{
+      "RecipeFormatVersion": "2020-01-25",
+      "ComponentName": "com.ml.xgboost",
+      "ComponentVersion": "`+version+`",
+      "ComponentDescription": "A component that runs a ML docker container from ECR.",
+      "ComponentPublisher": "Amazon",
+      "ComponentDependencies": {
+          "aws.greengrass.DockerApplicationManager": {
+              "VersionRequirement": "~2.0.0"
+          },
+          "aws.greengrass.TokenExchangeService": {
+              "VersionRequirement": "~2.0.0"
+          }
+      },
+      "ComponentConfiguration": {
+          "DefaultConfiguration": {
+              "accessControl": {
+                  "aws.greengrass.ipc.pubsub": {
+                      "com.ml.xgboost:pubsub:1": {
+                          "policyDescription": "Allows access to publish to all topics.",
+                          "operations": [
+                              "aws.greengrass#SubscribeToTopic"
+                          ],
+                          "resources": [
+                              "*"
+                          ]
+                      }
+                  }
+              }
+          }
+      },
+      "Manifests": [
+          {
+              "Platform": {
+                  "os": "all"
+              },
+              "Lifecycle": {
+                  "Run": "docker run `+imageUri+`"
+              },
+              "Artifacts": [
+                  {
+                    "URI": "docker:`+imageUri+`"
+                  }
+              ]
+          }
+      ]
+    }`
+    const cfnComponentVersion = new greengrassv2.CfnComponentVersion(this, 'MyCfnComponentVersion', {
+      inlineRecipe: recipe,
+    }); 
 
-    new cdk.CfnOutput(this, 'imageUri', {
-      value: asset.imageUri,
-      description: 'The image uri',
-    });
+    // deployments
+    const cfnDeployment = new greengrassv2.CfnDeployment(this, 'MyCfnDeployment', {
+      targetArn: `arn:aws:iot:ap-northeast-2:`+accountId+`:thing/`+deviceName,    
+      components: {
+        "com.ml.xgboost": {
+          componentVersion: version, 
+        },
+        "aws.greengrass.Cli": {
+          componentVersion: "2.8.1", 
+        }
+      },
+      deploymentName: 'ml-deployment',
+      deploymentPolicies: {
+        componentUpdatePolicy: {
+          action: 'NOTIFY_COMPONENTS', // NOTIFY_COMPONENTS | SKIP_NOTIFY_COMPONENTS
+          timeoutInSeconds: 60,
+        },
+        failureHandlingPolicy: 'ROLLBACK',  // ROLLBACK | DO_NOTHING
+      },
+    }); 
   }
 }
