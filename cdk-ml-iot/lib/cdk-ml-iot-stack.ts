3b78be91ca31533f6c7daf37913b7577323040ff
ksdyb.park <ksdyb@amazon.com>
2022-11-17 20:35:49 +0900

    update

--- cdk-ml-iot/lib/cdk-ml-iot-stack.ts
+++ cdk-ml-iot/lib/cdk-ml-iot-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as path from "path";
 import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
 import * as greengrassv2 from 'aws-cdk-lib/aws-greengrassv2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
 
 export class CdkMlIotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +12,106 @@ export class CdkMlIotStack extends cdk.Stack {
     
     const deviceName = 'GreengrassCore-18163f7ac3e'
     const accountId = cdk.Stack.of(this).account
+
+    // S3 for artifact storage
+    const s3Bucket = new s3.Bucket(this, "gg-depolyment-storage",{
+      bucketName: "gg-depolyment-storage",
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+    });
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: s3Bucket.bucketName,
+      description: 'The nmae of bucket',
+    });
+    new cdk.CfnOutput(this, 's3Arn', {
+      value: s3Bucket.bucketArn,
+      description: 'The arn of s3',
+    });
+    new cdk.CfnOutput(this, 's3Path', {
+      value: 's3://'+s3Bucket.bucketName,
+      description: 'The path of s3',
+    });    
+
+    // copy web application files into s3 bucket
+    new s3Deploy.BucketDeployment(this, "UploadArtifact", {
+      sources: [s3Deploy.Source.asset("../src")],
+      destinationBucket: s3Bucket,
+    });
+
+    // create container component - com.ml.xgboost
+    const version_xgboost = "1.0.0"
+    new containerComponent(scope, "container-component", version_xgboost)   
+
+    // create local component
+    const version_consumer = "1.0.0"
+    new localComponent(scope, "local-component", version_consumer, s3Bucket.bucketName)  
+
+    // deploy components
+    new componentDeployment(scope, "deployments", version_consumer, version_xgboost, accountId, deviceName)   
+  }
+}
+
+export class localComponent extends cdk.Stack {
+  constructor(scope: Construct, id: string, version: string, bucketName: string, props?: cdk.StackProps) {    
+    super(scope, id, props);
+
+    // recipe of component - com.ml.consumer
+    const recipe_consumer = `{
+      "RecipeFormatVersion": "2020-01-25",
+      "ComponentName": "com.ml.consumer",
+      "ComponentVersion": "${version}",
+      "ComponentDescription": "A component that consumes the API.",
+      "ComponentPublisher": "Amazon",
+      "ComponentConfiguration": {
+        "DefaultConfiguration": {
+          "accessControl": {
+            "aws.greengrass.ipc.pubsub": {
+              "com.ml.consumer:pubsub:1": {
+                "policyDescription": "Allows access to publish to all topics.",
+                "operations": [
+                  "aws.greengrass#PublishToTopic"
+                ],
+                "resources": [
+                  "*"
+                ]
+              }
+            }
+          }
+        }
+      },
+      "Manifests": [{
+        "Platform": {
+          "os": "linux"
+        },
+        "Lifecycle": {
+          "Install": "pip3 install awsiotsdk pandas",
+          "Run": "python3 -u {artifacts:path}/consumer.py"
+        },
+        "Artifacts": [
+          {
+            "URI": "${'s3://'+bucketName}/consumer/artifacts/com.ml.consumer/1.0.0/consumer.py"
+          },
+          {
+            "URI": "${'s3://'+bucketName}/consumer/artifacts/com.ml.consumer/1.0.0/samples.json"
+          }
+        ]
+      }]
+    }`
+
+    // recipe of component - com.ml.consumer
+    new greengrassv2.CfnComponentVersion(this, 'MyCfnComponentVersion-Consumer', {
+      inlineRecipe: recipe_consumer,
+    });        
+  }
+}
+
+
+export class containerComponent extends cdk.Stack {
+  constructor(scope: Construct, id: string, version: string, props?: cdk.StackProps) {    
+    super(scope, id, props);
 
     const asset = new DockerImageAsset(this, 'BuildImage', {
       directory: path.join(__dirname, '../../src/ml-container'),
@@ -22,7 +124,6 @@ export class CdkMlIotStack extends cdk.Stack {
     }); 
 
     // recipe of component - com.ml.xgboost
-    const version = "1.0.0"
     const recipe = `{
       "RecipeFormatVersion": "2020-01-25",
       "ComponentName": "com.ml.xgboost",
@@ -60,7 +161,7 @@ export class CdkMlIotStack extends cdk.Stack {
             "os": "all"
           },
           "Lifecycle": {
-            "Run": "docker run ${imageUri} -v \$AWS_GG_NUCLEUS_DOMAIN_SOCKET_FILEPATH_FOR_COMPONENT:\$AWS_GG_NUCLEUS_DOMAIN_SOCKET_FILEPATH_FOR_COMPONENT -e SVCUID -e AWS_GG_NUCLEUS_DOMAIN_SOCKET_FILEPATH_FOR_COMPONENT"
+            "Run": "docker run ${imageUri}"
           },
           "Artifacts": [
             {
@@ -71,23 +172,47 @@ export class CdkMlIotStack extends cdk.Stack {
       ]
     }`
 
-    const cfnComponentVersion = new greengrassv2.CfnComponentVersion(this, 'MyCfnComponentVersion', {
+    const cfnComponentVersion = new greengrassv2.CfnComponentVersion(this, 'MyCfnComponentVersion_Container', {
       inlineRecipe: recipe,
     }); 
-    // cfnComponentVersion.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
+  }
+}
+
+export class componentDeployment extends cdk.Stack {
+  constructor(scope: Construct, id: string, version_consumer: string, version_xgboost: string, accountId: string, deviceName: string, props?: cdk.StackProps) {    
+    super(scope, id, props);
 
     // deployments
     const cfnDeployment = new greengrassv2.CfnDeployment(this, 'MyCfnDeployment', {
       targetArn: `arn:aws:iot:ap-northeast-2:`+accountId+`:thing/`+deviceName,    
       components: {
+        "com.ml.consumer": {
+          componentVersion: version_consumer, 
+        }, 
         "com.ml.xgboost": {
-          componentVersion: version, 
-        },
+          componentVersion: version_xgboost, 
+        },  
         "aws.greengrass.Cli": {
-          componentVersion: "2.8.1", 
-        }
+          componentVersion: "2.9.0", 
+        },
+        "aws.greengrass.LegacySubscriptionRouter": {
+          componentVersion: "2.1.8", 
+          configurationUpdate: {
+            merge: `{
+              "subscriptions": {
+                "com.ml.consumer": {
+                  "id": "Greengrass_Container_Consumer",
+                  "source": "component:com.ml.consumer",
+                  "subject": "local/topic",
+                  "target": "component:com.ml.container"   
+                }
+              }
+            }`,    // target: cloud or lambda component name(component:com.ml.HelloWorldLambda) or ARN of a Lambda function
+            reset: [],
+          }, 
+        } 
       },
-      deploymentName: 'ml-deployment',
+      deploymentName: 'component-deployment',
       deploymentPolicies: {
         componentUpdatePolicy: {
           action: 'NOTIFY_COMPONENTS', // NOTIFY_COMPONENTS | SKIP_NOTIFY_COMPONENTS
@@ -95,7 +220,6 @@ export class CdkMlIotStack extends cdk.Stack {
         },
         failureHandlingPolicy: 'ROLLBACK',  // ROLLBACK | DO_NOTHING
       },
-    }); 
-    // cfnDeployment.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
+    });   
   }
 }
