1cc11a05c86d51b388b24ee82fc1644f234f37ff
ksdyb.park <ksdyb@amazon.com>
2022-11-27 11:00:13 +0900

    update

--- cdk-ml-iot/lib/cdk-ml-iot-stack.ts
+++ cdk-ml-iot/lib/cdk-ml-iot-stack.ts
@@ -193,12 +193,12 @@ export class componentDeployment extends cdk.Stack {
         "com.ml.consumer": {
           componentVersion: version_consumer 
         }, 
-        "com.ml.xgboost": {
+      /*  "com.ml.xgboost": {
           componentVersion: version_xgboost
         },  
         "aws.greengrass.Cli": {
           componentVersion: "2.9.0", 
-        }
+        } */
       },
       deploymentName: 'component-deployment',
       deploymentPolicies: {
